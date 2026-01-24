50d2fa13a1a37d2e4d9824160957e15eaad1fa56
GauravKumarCh0105 <gauravkumarc@kapilit.com>
2026-01-24 16:35:58 +0530

    modifying accounting reports

--- src/app/components/ACCOUNTS REPORTS/cheque-enquiry/cheque-enquiry.component.ts
+++ src/app/components/ACCOUNTS REPORTS/cheque-enquiry/cheque-enquiry.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cheque-enquiry',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './cheque-enquiry.component.html',
   styleUrl: './cheque-enquiry.component.css',
 })
 export class ChequeEnquiryComponent {
+chequeType: string = 'issued';
 
 }
