27ac37c9d90c35ec57f72b1ba0e4b6541ffd8a18
gowthamikakula <gowthamikakula@kapilit.com>
2026-02-03 19:21:25 +0530

    latest

--- src/app/components/ACCOUNTS REPORTS/cash-book/cash-book.component.ts
+++ src/app/components/ACCOUNTS REPORTS/cash-book/cash-book.component.ts
@@ -1,114 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators ,FormsModule} from '@angular/forms';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { BsDatepickerConfig, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
 @Component({
   selector: 'app-cash-book',
-  imports: [BsDatepickerModule, NgxDatatableModule, CommonModule, FormsModule],
-   templateUrl: './cash-book.component.html',
-  styleUrl: './cash-book.component.css',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgxDatatableModule,
+    BsDatepickerModule
+  ],
+  templateUrl: './cash-book.component.html',
+  styleUrl: './cash-book.component.css'
 })
-export class CashBookComponent 
-// implements OnInit 
-{today:Date=new Date();
-  fromDate=this.today;
-  toDate=this.today;
- public dpConfig: Partial<BsDatepickerConfig> = new BsDatepickerConfig();
-constructor(private fb:FormBuilder){
-  this.dpConfig.maxDate = new Date();
-    this.dpConfig.containerClass = 'theme-dark-blue';
-    this.dpConfig.dateInputFormat = 'DD-MMM-YYYY';
-    this.dpConfig.showWeekNumbers = false;
+export class CashBookComponent implements OnInit {
+
+  today: Date = new Date();
+  fromDate: Date = this.today;
+  toDate: Date = this.today;
+
+  transactionType: string = '';
+
+  show = true;
+
+  gridView: any[] = [];
+
+  dpConfig: Partial<BsDatepickerConfig> = {};
+
+  ngOnInit(): void {
+    this.dpConfig = {
+      maxDate: new Date(),
+      containerClass: 'theme-dark-blue',
+      dateInputFormat: 'DD-MMM-YYYY',
+      showWeekNumbers: false
+    };
+  }
+
+  GenerateReport() {
+    if (!this.fromDate || !this.toDate || !this.transactionType) {
+      alert('Please select From Date, To Date and Transaction Type');
+      return;
+    }
+
+    this.show = false;
+    this.gridView = this.getDummyData();
+  }
+
+  getDummyData() {
+    return [
+      { ptransactiondate: '01-Feb-2026', description: 'Opening Balance', amount: 10000 },
+      { ptransactiondate: '04-Feb-2026', description: 'Cash Receipt', amount: 5000 },
+      { ptransactiondate: '05-Feb-2026', description: 'Office Expense', amount: -2000 },
+      { ptransactiondate: '06-Feb-2026', description: 'Cash Receipt', amount: 3000 }
+    ];
+  }
+
+  pdfOrprint(type: 'Pdf' | 'Print') {
+    if (type === 'Print') {
+      window.print();
+    } else {
+      alert('PDF export not implemented');
+    }
+  }
+
+  exportExcel() {
+    alert('Excel export not implemented');
+  }
 }
-  show: boolean = false
- ngOnInit() {
-  
- 
-}
-
-
-}
-
-  
-  
-
-  // CashBookReportForm!: FormGroup;
-  // submitted = false;
-  // savebutton = 'Generate';
-
-  // loading = false;
-  // showicons = false;
-  // printedDate = true;
-
-  // startDate: any;
-  // endDate: any;
-
-  // gridView: any[] = [];
-
-  // pageCriteria = {
-  //   pageSize: 10,
-  //   offset: 0,
-  //   footerPageHeight: 50
-  // };
-
-  // dpConfig = {
-  //   containerClass: 'theme-default',
-  //   dateInputFormat: 'DD/MM/YYYY'
-  // };
-
-  // dpConfig1 = {
-  //   containerClass: 'theme-default',
-  //   dateInputFormat: 'DD/MM/YYYY'
-  // };
-
-
-
-  // get f() {
-  //   return this.CashBookReportForm.controls;
-  // }
-
-  // ToDateChange(event: any) {
-  //   this.endDate = event;
-  // }
-
-  // FromDateChange(event: any) {
-  //   this.startDate = event;
-  // }
-
-  // cashBookData() {
-  //   this.submitted = true;
-
-  //   if (this.CashBookReportForm.invalid) {
-  //     return;
-  //   }
-
-  //   this.loading = true;
-
-  //   // dummy static data
-  //   this.gridView = [
-  //     { ptransactiondate: '2024-01-01', description: 'Cash In', amount: 100 },
-  //     { ptransactiondate: '2024-01-01', description: 'Cash Out', amount: -50 },
-  //     { ptransactiondate: '2024-01-02', description: 'Cheque In', amount: 200 }
-  //   ];
-
-  //   this.showicons = true;
-  //   this.loading = false;
-  // }
-
-  // toggleExpandGroup(group: any) {
-  //   group.expanded = !group.expanded;
-  // }
-
-  // onDetailToggle(event: any) {
-  //   console.log(event);
-  // }
-
-  // pdfOrprint(type: string) {
-  //   console.log(type);
-  // }
-
-  // export() {
-  //   console.log('Export');
-  // }
-
