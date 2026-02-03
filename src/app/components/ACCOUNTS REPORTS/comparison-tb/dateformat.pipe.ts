d5849b8717b74a6d945bd738071bfd999483339f
GauravKumarCh0105 <gauravkumarc@kapilit.com>
2026-02-03 10:09:52 +0530

    changes

--- src/app/components/ACCOUNTS REPORTS/comparison-tb/dateformat.pipe.ts
+++ src/app/components/ACCOUNTS REPORTS/comparison-tb/dateformat.pipe.ts
@@ -8,7 +8,8 @@ export class DateformatPipe implements PipeTransform {
   transform(value: Date | string): string {
     const d = new Date(value);
     const day = ('0' + d.getDate()).slice(-2);
-    const month = ('0' + (d.getMonth() + 1)).slice(-2);
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const month = months[d.getMonth()];
     return `${day}-${month}-${d.getFullYear()}`;
   }
 
