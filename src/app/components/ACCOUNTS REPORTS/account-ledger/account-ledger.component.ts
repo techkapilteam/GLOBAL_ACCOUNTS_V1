6b196558e08072764506d42d9ef12a1c19f5c631
GauravKumarCh0105 <gauravkumarc@kapilit.com>
2026-02-19 12:45:48 +0530

    ledger

--- src/app/components/ACCOUNTS REPORTS/account-ledger/account-ledger.component.ts
+++ src/app/components/ACCOUNTS REPORTS/account-ledger/account-ledger.component.ts
@@ -141,7 +141,7 @@ today = new Date();
 
     if (!ledgerId) return;
 
-    this.transactionService.GetSubLedgerData(ledgerId)
+    this.transactionService.GetSubLedgerData(ledgerId,'accounts','KAPILCHITS','accounts','KLC01','global')
       .subscribe({
         next: (res) => this.subledgeraccountslist = res ?? [],
         error: (err) => this.commonService.showErrorMessage(err)
